230f2ea5bc41eaa968a86808a5b3bf7ad7567d89
Sergio Conde Gomez <sconde@carto.com>
2019-05-08 17:15:07 +0200

    Initial commit

---
+++ bamboohr-timesheet.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         BambooHR Timesheet Fill
+// @namespace    bamboohr.sconde.net
+// @version      0.3
+// @description  Fill BambooHR Timesheet with templates
+// @author       Sergio Conde
+// @match        https://*.bamboohr.com/*
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @updateURL    https://raw.githubusercontent.com/skgsergio/bamboohr-timesheet-greasemonkey/master/bamboohr-timesheet.user.js
+// ==/UserScript==
+
+'use strict';
+
+/*
+   Don't touch this, won't persist across updates.
+
+   Load BambooHR for the first time with the script and then open this script Storage preferences and edit there.
+ */
+const DEFAULT_TEMPLATES = {
+  'Mon-Thu': [{ start: '8:30', end: '13:00' }, { start: '13:30', end: '17:00' }],
+  'Fri': [{ start: '8:30', end: '14:30' }, { start: '15:30', end: '17:30' }]
+};
+
+function htmlToElement(html) {
+  let template = document.createElement('template');
+  template.innerHTML = html.trim();
+  return template.content.firstChild;
+}
+
+(async function() {
+  let TEMPLATES = await GM.getValue('TEMPLATES');
+
+  if (!TEMPLATES) {
+    TEMPLATES = DEFAULT_TEMPLATES;
+    GM.setValue('TEMPLATES', TEMPLATES);
+  }
+
+  for (const template of Object.keys(TEMPLATES)) {
+    let link = htmlToElement(`<li><a href="#" data-template="${template}">Fill Timesheet: ${template}</a></li>`);
+
+    link.querySelector('a').onclick = function () {
+      let now = new Date();
+      // Do JS have propper date formatting? :facepalm:
+      let date = prompt("Please enter the date", `${now.getFullYear()}-${('0' + (now.getMonth() + 1)).slice(-2)}-${('0' + now.getDate()).slice(-2)}`);
+
+      if (!date) {
+        alert("Canceled!");
+        return false;
+      }
+
+      let entries = [];
+      for (const [idx, slot] of TEMPLATES[this.dataset.template].entries()) {
+        entries.push({
+          id: null,
+          trackingId: idx + 1,
+          employeeId: window.SESSION_USER.employeeId,
+          date: date,
+          start: slot.start,
+          end: slot.end,
+          note: ''
+        });
+      }
+
+      fetch(
+        `${window.location.origin}/timesheet/clock/entries`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          referrer: 'client',
+          headers: {
+            'content-type': 'application/json; charset=UTF-8',
+            'x-csrf-token': window.CSRF_TOKEN
+          },
+          body: JSON.stringify({ entries: entries })
+        }
+      ).then(data => {
+        if (data.status == 200) {
+          alert('Done!');
+        } else {
+          data.text().then(t => alert(`Request error!\nHTTP Code: ${data.status}\nResponse:\n${t}`));
+        }
+      }).catch(err => alert(`Fetch error!\n\n${err}`));
+
+      return false;
+    };
+
+    window.document.querySelector('ul#nav-tabs').append(link);
+  }
+})();
